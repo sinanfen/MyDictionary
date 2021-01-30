1ea73ad4bf2f9e2f0c5d2e708e1f9580644a258a
Sinan <sinanfen@hotmail.com>
2021-01-30 15:37:33 +0300

    deneme

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> MyDictionary = new MyDictionary<string, int>();
+            MyDictionary.Add("Episode One", 51);
+            MyDictionary.Add("Episode Two", 57);
+            MyDictionary.Add("Episode Three", 55);
+            MyDictionary.Add("Episode Four", 58);
+            MyDictionary.Add("Episode Five", 51);
+
+            Console.WriteLine("Season One : " + MyDictionary.Keys[0] + "  Time: " + MyDictionary.Values[0]);
+            Console.WriteLine("Season One : " + MyDictionary.Keys[1] + "  Time: " + MyDictionary.Values[1]);
+            Console.WriteLine("Season One : " + MyDictionary.Keys[1] + "  Time: " + MyDictionary.Values[2]);
+            Console.WriteLine("Season One : " + MyDictionary.Keys[1] + "  Time: " + MyDictionary.Values[3]);
+            Console.WriteLine("Season One : " + MyDictionary.Keys[1] + "  Time: " + MyDictionary.Values[4]);
+
+            Console.WriteLine("Total Episodes Number: " + MyDictionary.Values.Length);
+        }
+    }
+
+}
